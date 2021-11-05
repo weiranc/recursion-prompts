de2be88c9b9c8a80e59ba537993f561ea0f85caf
Weiran Cheng <chengwr1128@gmail.com>
2021-11-05 14:53:54 -0700

    Completed 11 and 12

--- src/recursion.js
+++ src/recursion.js
@@ -158,12 +158,70 @@ var palindrome = function(string) {
 // modulo(5,2) // 1
 // modulo(17,5) // 2
 // modulo(22,6) // 4
+
+
 var modulo = function(x, y) {
+
+  if (y === 0) {
+    return NaN;
+  }
+  if (x >= 0 && y >= 0) {
+    if (x < y) {
+      return x;
+    } else {
+      return modulo(x - y, y);
+    }
+  } else if (x < 0 && y > 0) {
+    if (-x < y) {
+      return x;
+    } else {
+      return modulo(x + y, y);
+    }
+  } else if (x > 0 && y < 0) {
+    if (x < -y) {
+      return -x;
+    } else {
+      return modulo(x + y, y);
+    }
+  } else {
+    if (-x < -y) {
+      return x;
+    } else {
+      return modulo(x - y, y);
+    }
+  }
 };
 
 // 12. Write a function that multiplies two numbers without using the * operator or
 // Math methods.
 var multiply = function(x, y) {
+  if (x === 0 || y === 0) {
+    return 0;
+  } if ( x > 0 && y > 0){
+    if (y === 1) {
+      return x;
+    } else {
+      return x + multiply(x, y - 1);
+    }
+  } else if (x < 0 && y < 0) {
+    if ( y === -1) {
+      return -x;
+    } else {
+      return -x + multiply(x, y + 1);
+    }
+  } else if (x > 0 && y < 0) {
+    if ( y === -1) {
+      return -x;
+    } else {
+      return -x + multiply(x, y + 1);
+    }
+  } else {
+    if ( y === 1) {
+      return x;
+    } else {
+      return x + multiply(x, y - 1);
+    }
+  }
 };
 
 // 13. Write a function that divides two numbers without using the / operator or
